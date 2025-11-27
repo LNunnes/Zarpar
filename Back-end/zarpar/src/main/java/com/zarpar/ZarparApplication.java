f488ab630cc0420e08535f3e68f3142b466684a0
Lucas Nunes <lucasnunnes40@gmail.com>
2025-11-27 20:02:40 -0300

    Base: Criando repositorio back-end

---
+++ Back-end/zarpar/src/main/java/com/zarpar/ZarparApplication.java
@@ -0,0 +1,13 @@
+package com.zarpar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZarparApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZarparApplication.class, args);
+	}
+
+}
---
+++ Back-end/zarpar/src/test/java/com/zarpar/ZarparApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zarpar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZarparApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
