42d616fcd46d49c00c25e28f47ffbda18f51bbc7
Lucas Nunes <lucasnunnes40@gmail.com>
2025-12-05 18:46:12 -0300

    ISSUE-2: Criando frontend para operar os pontos turisticos

--- Front-end/zarpar/src/app/app.routes.ts
+++ Front-end/zarpar/src/app/app.routes.ts
@@ -2,10 +2,25 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login';
 import { RegisterComponent } from './pages/register/register';
 import { HomeComponent } from './pages/home/home';
+import { PontoFormComponent } from './pages/ponto-form/ponto-form.component';
+import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
+
+    {
+      path: 'pontos/novo',
+      component: PontoFormComponent,
+      canActivate: [authGuard]
+    },
+
+    {
+      path: 'pontos/editar/:id',
+      component: PontoFormComponent,
+      canActivate: [authGuard]
+    },
+
     { path: '**', redirectTo: '' }
 ];
---
+++ Front-end/zarpar/src/app/core/services/ponto.service.ts
@@ -0,0 +1,37 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Page, PontoRequest, PontoTuristico } from '../models/ponto.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PontoService {
+
+  private http = inject(HttpClient);
+  private apiUrl = 'http://localhost:8080/api/pontos';
+
+  listar(page: number = 0, size: number = 10): Observable<Page<PontoTuristico>> {
+    let params = new HttpParams()
+      .set('page', page.toString())
+      .set('size', size.toString());
+
+    return this.http.get<Page<PontoTuristico>>(this.apiUrl, { params });
+  }
+
+  buscarPorId(id: number): Observable<PontoTuristico> {
+    return this.http.get<PontoTuristico>(`${this.apiUrl}/${id}`);
+  }
+
+  criar(dados: PontoRequest): Observable<PontoTuristico> {
+    return this.http.post<PontoTuristico>(this.apiUrl, dados);
+  }
+
+  atualizar(id: number, dados: PontoRequest): Observable<PontoTuristico> {
+    return this.http.put<PontoTuristico>(`${this.apiUrl}/${id}`, dados);
+  }
+
+  excluir(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
